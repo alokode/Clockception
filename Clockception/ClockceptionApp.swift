b34c23c229164c2bd477f6d19d789926210b8632
alok_sagarp <alok.sagarp@gmail.com>
2022-02-10 19:15:37 +0530

    First commit

---
+++ Clockception/ClockceptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockceptionApp.swift
+//  Clockception
+//
+//  Created by Alok Sagar on 10/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ClockceptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
